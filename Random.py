b2406463183e64e8700d8b85c375aec6b6de1647
Mr.Devil <87979690+mrperfect0056@users.noreply.github.com>
2021-10-28 22:59:30 +0600

    Update Random.py

--- Random.py
+++ Random.py
@@ -80,15 +80,15 @@ vuln = "\033[92mVuln"
 
 os.system("clear")
 print  """
-\033[1;92m
+\033[1;92m_          _                                                           \        /                                                           __\______/__
 \033[1;92m    | [©]  [©] |​
-\033[1;92m
+\033[1;92m    |  [====]  | [+] DARK_MIND HACKER  [+]
 \033[1;92m╔══o00════════00o═════════════════════════╗
-\033[1;92m█  [•] Author:DARK_DEVIL                  █
-\033[1;92m█  [•] Faceboook:darkhacker07                 █
-\033[1;92m█  [•] Whatsapp:1875529351_                 █
-\033[1;92m█  [•] Youtube:UCXItn4AnlxExhW0virHuD3Q     █
-\033[1;92m█  [•] Version:0.3                         █
+\033[1;92m█  [•] Author:DARK_DEVIL              █
+\033[1;92m█  [•] Faceboook:darkhacker07                █
+\033[1;92m█  [•] Whatsapp:1875529351_                █
+\033[1;92m█  [•] Youtube:UCXItn4AnlxExhW0virHuD3Q    █
+\033[1;92m█  [•] Version:0.3                        █
 \033[1;92m╚═════════════════════════════════════════╝
 """
 
